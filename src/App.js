91d50bbeae94f987fd37c305b0bb7e2f151eaf2e
HossainRafi <hossainrafi71@gmail.com>
2022-05-22 14:44:41 +0600

    navbar modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Navbar from "./pages/Shared/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Blogs from './pages/Blogs/Blogs';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Navbar from './pages/Shared/Navbar';
 
 
 function App() {
   return (
     <div>
-        <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
     </div>
   );
 }
