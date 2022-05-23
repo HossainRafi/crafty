aa7891e3e65f595b0ed5c51570c5df98d41382fb
HossainRafi <hossainrafi71@gmail.com>
2022-05-23 13:49:19 +0600

    product done

--- src/pages/Home/Product.js
+++ src/pages/Home/Product.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const Product = ({product}) => {
-    const { name, img, minquantity, maxquantity, desc, price } = product;
-    return (
-      <div className="card bg-gray-50">
-        <figure className="px-5 pt-5">
-          <img
-            src={img}
-            alt=""
-            className="rounded-xl w-[230px] h-[190px]"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title text-2xl mx-auto font-bold">
-            {name}
-          </h2>
-          <p className="text-justify">{desc}</p>
-          <p className="text-justify font-bold">Available Product: {maxquantity}</p>
-          <p className="text-justify font-bold">Minimum Order: {minquantity}</p>
-          <p className="text-justify font-bold">Per Product Price: ${price}</p>
-        </div>
+const Product = ({ product }) => {
+  const { name, img, minquantity, maxquantity, desc, price } = product;
+  return (
+    <div className="card bg-gray-50">
+      <figure className="px-5 pt-5">
+        <img src={img} alt="" className="rounded-xl w-[230px] h-[190px]" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-2xl mx-auto font-bold">{name}</h2>
+        <p className="text-justify">{desc}</p>
+        <p className="font-bold">Available Product: {maxquantity}</p>
+        <p className="font-bold">Minimum Order: {minquantity}</p>
+        <p className="font-bold">Per Product Price: ${price}</p>
       </div>
-    );
+      <div className="mx-auto pb-5">
+        <button class="btn btn-md w-32 btn-outline bg-gray-600 text-white border-0">
+          Purchase
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/pages/Home/Products.js
+++ src/pages/Home/Products.js
@@ -12,11 +12,11 @@ const Products = () => {
   return (
     <div className="pt-24 pb-16">
       <div className="text-center">
-        <h3 className="text-secondary text-3xl  font-bold uppercase">
+        <h3 className="text-blue-900 text-4xl font-bold font-serif uppercase">
           Product List
         </h3>
       </div>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-10 px-10">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-10">
         {products.map((product) => (
           <Product key={product._id} product={product}></Product>
         ))}
